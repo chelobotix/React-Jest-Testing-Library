96014218d97eab78f1d0c7ec991da96edeee4ffa
Marcelo <marceloalarconbarrenechea@gmail.com>
2023-08-17 15:39:20 -0400

    Jest Configured

--- src/__test__/App.snapshot.test.js
+++ src/__test__/App.snapshot.test.js
@@ -1,8 +1,8 @@
 import renderer from 'react-test-renderer'
 
-import App from './App'
+import App from '../App'
 
 test('renders learn react link', () => {
     const tree = renderer.create(<App />).toJSON()
-    expect(tree).toMatchInlineSnapshot()
+    expect(tree).toMatchInlineSnapshot(`null`)
 })
